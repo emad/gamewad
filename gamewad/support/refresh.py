d3148ec742286301964b51d210b98cde3cfd0c34
emad <elharaty@gmail.com>
2009-01-17 04:10:07 -0500

    better game zip update detection

--- gamewad/support/refresh.py
+++ gamewad/support/refresh.py
@@ -7,7 +7,7 @@ def main(fn):
     data = simplejson.load(f)
     games = data['games']
     for g in games:
-        zfn = "../priv/www/static/games/" + os.path.basename(g['zip_url'])
+        zfn = "../priv/www/static/games/" + os.path.basename(os.path.splitext(g['zip_url'])[0])
         if not os.path.exists(zfn):
             print "curl -O -C -", g['zip_url']
             print "unzip ", os.path.basename(g['zip_url'])
